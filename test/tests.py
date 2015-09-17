f3ed34f1cd7deb9b6f5462442504b06aa7c1667f
Yonatan Romero <yromero@openmailbox.org>
2015-09-17 15:19:12 -0300

    Implementacion de metodo "confimar compra". Devuelve que el servicio no esta habilitado

--- test/tests.py
+++ test/tests.py
@@ -17,12 +17,16 @@ CONTRATO_ESTANDAR = "AND00EST"
 # Contrato gestion de cambio
 CONTRATO_CAMBIO = "AND00CMB"
 
-logging.basicConfig(filename='testing.log', level=logging.DEBUG)
+logging.basicConfig(filename='testing.log', filemode='w', level=logging.DEBUG)
 
 
 class AndreaniTests(TestCase):
 
     def test_consultar_sucursales(self):
+        '''
+        Prueba servicios de consultar sucursales habilitadas para el retiro de
+        paquetes de e-commerce.
+        '''
         andreani = Andreani(TEST_USER, TEST_PASSWD, CLIENTE, CONTRATO_SUCURSAL)
         # consulta sin filtros
         sucursales = andreani.consulta_sucursales()
@@ -57,6 +61,9 @@ class AndreaniTests(TestCase):
         self.assertFalse(sucursales)
 
     def test_cotizar_envio(self):
+        '''
+        Prueba servicios de cotizar envios.
+        '''
         # cotizacion envio a domicilio
         andreani = Andreani(TEST_USER, TEST_PASSWD, CLIENTE, CONTRATO_ESTANDAR)
         cotizacion =  andreani.cotizar_envio(cp_destino="9410", # ushuaia
@@ -89,3 +96,42 @@ class AndreaniTests(TestCase):
             cotizacion =  andreani.cotizar_envio(cp_destino="1001",
                                                  peso="10",
                                                  volumen="0")
+    def test_confirmar_compra(self):
+        '''
+        Prueba servicio de confirmar compra.
+        '''
+        parametros = { 
+            'sucursal_retiro': '20',
+            'provincia': 'Buenos Aires',
+            'localidad': 'San Justo',
+            'codigo_postal': '1754',
+            'calle': 'Florencio Varela',
+            'numero': '1903',
+            'departamento': '',
+            'piso': '',
+            'nombre_apellido': 'Susana Horia',
+            'tipo_documento': 'DNI',
+            'numero_documento': '12345678',
+            'email': 'email@example.com',
+            'numero_celular': '011 15 1515 1111',
+            'numero_telefono': '011 4321 1234',
+            'nombre_apellido_alternativo': 'Susi',
+            'numero_transaccion': '',
+            'detalle_productos_entrega': 'Prueba de entrega',
+            'detalle_productos_retiro': 'Prueba de envio',
+            'peso': '1000',
+            'volumen': '1000',
+            'valor_declarado': '0.5',
+            'valor_cobrar': '1.0',
+            'contrato': '',
+            'sucursal_cliente': '',
+            'categoria_distancia': '',
+            'categoria_facturacion': '',
+            'categoria_peso': '',
+            'tarifa': '20.0',
+        }
+        # confirmacion de compra con cotizacion de envio
+        andreani = Andreani(TEST_USER, TEST_PASSWD, CLIENTE, CONTRATO_ESTANDAR)
+        compra = andreani.confirmar_compra(**parametros)
+        self.assertTrue(compra)
+        print(compra)
