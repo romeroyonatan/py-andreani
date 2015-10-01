5fb2003858ba8e8eada6b220a5ff849b874358d6
Yonatan Romero <yromero@openmailbox.org>
2015-10-01 10:13:07 -0300

    Cambio de version.
    Simplificado manejo de dependencias

--- setup.py
+++ setup.py
@@ -1,25 +1,15 @@
 from setuptools import setup
 
-from pip.req import parse_requirements
-from pip.download import PipSession
-
-# parse_requirements() returns generator of pip.req.InstallRequirement objects
-install_reqs = parse_requirements("requirements.txt", session=PipSession())
-
-# reqs is a list of requirement
-# e.g. ['django==1.5.1', 'mezzanine==1.4.6']
-reqs = [str(ir.req) for ir in install_reqs]
-
 setup(name='py-andreani',
-      version='0.1',
+      version='0.2beta',
       description='Implementa servicios ofrecidos por API de Andreani',
       author='Yonatan Romero',
       author_email='yromero@openmailbox.org',
       license='GPLv2',
       url='https://github.com/romeroyonatan/py-andreani',
       platforms="Python3",
-      keywords="andreani e-commerce argentina oca",
+      keywords="andreani e-commerce ecommerce argentina oca",
       packages=['andreani'],
-      install_requires=reqs,
+      install_requires="suds-jurko",
       test_suite="test",
-      )
+)
